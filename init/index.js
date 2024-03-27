c3207618d94b5f98d1fed362b7b1839e89c63c85
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-03-27 23:29:03 +0530

    commit message

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(()=>{
+        console.log("connected to DB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({}); // delete all listings from the database
+    await Listing.insertMany(initData.data);
+    console.log("data was initilizd");
+};
+
+initDB();
+
